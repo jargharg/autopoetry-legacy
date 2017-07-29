56a9bcb7b320da3e2e2353ce2e951595a3d456ec
Jarod <jarod.hargreaves@gmail.com>
2017-07-29 17:41:15 +0100

    Fix possible memory leak

--- app/assets/js/modules/TopNav.js
+++ app/assets/js/modules/TopNav.js
@@ -3,15 +3,17 @@ class TopNav {
 		this.topNav = $(".topNav")
 		this.events()
 	}
+
 	events() {
 		this.topNav.click(this.infoToggle.bind(this))
 	}
+
 	infoToggle(e){
 		$(".appDetails").toggleClass("visible")
 		e.stopPropagation()
 		$(document).click(function(){
 			$(".appDetails").removeClass("visible")
-			$("body").off()
+			$(document).off()
 		})
 	}
 }
