5537302ff18db8c01eafa5ad531168ec2b5fabe6
Jarod <jarod.hargreaves@gmail.com>
2017-08-26 10:41:46 +0100

    Remove semicolons for consistency

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -4,47 +4,63 @@ var gulp = require("gulp"),
 	rev = require("gulp-rev"),
 	cssnano = require("gulp-cssnano"),
 	uglify = require("gulp-uglify"),
-	browserSync = require("browser-sync").create();
+	browserSync = require("browser-sync").create()
 
-gulp.task("previewDocs", function(){
+gulp.task("previewDocs", function() {
 	browserSync.init({
 		notify: false,
 		server: {
 			baseDir: "docs"
 		}
-	});
-});
+	})
+})
 
 gulp.task("deleteDocsFolder", function() {
-	return del("./docs");
-});
+	return del("./docs")
+})
 
 gulp.task("copyGeneralFiles", ["deleteDocsFolder"], function() {
 	var pathsToCopy = [
-	"./app/**/*",
-	"!./app/index.html",
-	"!./app/assets/js",
-	"!./app/assets/js/**",
-	"!./app/assets/css",
-	"!./app/assets/css/**",
-	"!./app/temp",
-	"!./app/temp/**"
-	];
-	return gulp.src(pathsToCopy)
-	.pipe(gulp.dest("./docs"));
-});
+		"./app/**/*",
+		"!./app/index.html",
+		"!./app/assets/js",
+		"!./app/assets/js/**",
+		"!./app/assets/css",
+		"!./app/assets/css/**",
+		"!./app/temp",
+		"!./app/temp/**"
+	]
+	return gulp.src(pathsToCopy).pipe(gulp.dest("./docs"))
+})
 
-gulp.task("useminTrigger", ["deleteDocsFolder"], function(){
-	gulp.start("usemin");
-});
+gulp.task("useminTrigger", ["deleteDocsFolder"], function() {
+	gulp.start("usemin")
+})
 
 gulp.task("usemin", ["styles", "scripts"], function() {
-	return gulp.src("./app/index.html")
-	.pipe(usemin({
-		css: [function(){return rev();}, function(){return cssnano();}],
-		js: [function(){return rev();}, function(){return uglify();}]
-	}))
-	.pipe(gulp.dest("./docs"));
-});
+	return gulp
+		.src("./app/index.html")
+		.pipe(
+			usemin({
+				css: [
+					function() {
+						return rev()
+					},
+					function() {
+						return cssnano()
+					}
+				],
+				js: [
+					function() {
+						return rev()
+					},
+					function() {
+						return uglify()
+					}
+				]
+			})
+		)
+		.pipe(gulp.dest("./docs"))
+})
 
-gulp.task("build", ["deleteDocsFolder", "useminTrigger", "copyGeneralFiles"]);
+gulp.task("build", ["deleteDocsFolder", "useminTrigger", "copyGeneralFiles"])
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,33 +1,34 @@
 var gulp = require("gulp"),
 	watch = require("gulp-watch"),
-	browserSync = require("browser-sync").create();
+	browserSync = require("browser-sync").create()
 
-gulp.task("watch", ["scriptsRefresh", "cssInject"], function(){
+gulp.task("watch", ["scriptsRefresh", "cssInject"], function() {
 	browserSync.init({
 		notify: false,
 		server: {
 			baseDir: "./app"
 		}
-	});
-	
+	})
+
 	watch("./app/index.html", function() {
-		browserSync.reload();
-	});
-	
-	watch("./app/assets/css/**/*.css", function(){
-		gulp.start("cssInject");
-	});
+		browserSync.reload()
+	})
 
-	watch("./app/assets/js/**/*.js", function(){
-		gulp.start("scriptsRefresh");
-	});
-});
+	watch("./app/assets/css/**/*.css", function() {
+		gulp.start("cssInject")
+	})
 
-gulp.task("cssInject", ["styles"], function(){
-	return gulp.src("./app/temp/assets/css/styles.css")
-	.pipe(browserSync.stream());
-});
+	watch("./app/assets/js/**/*.js", function() {
+		gulp.start("scriptsRefresh")
+	})
+})
 
-gulp.task("scriptsRefresh", ["scripts"], function(){
-	browserSync.reload();
-});
+gulp.task("cssInject", ["styles"], function() {
+	return gulp
+		.src("./app/temp/assets/css/styles.css")
+		.pipe(browserSync.stream())
+})
+
+gulp.task("scriptsRefresh", ["scripts"], function() {
+	browserSync.reload()
+})
